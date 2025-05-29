09ac07cd2c2593aea91ba7dd3795c28fb98bcdff
Saiyeggoni2004 <yeggonisai@gmail.com>
2025-05-29 09:48:08 +0530

    Add files via upload

---
+++ pythonbeginner-2.py
@@ -0,0 +1,47 @@
+a=12
+if a>=16:
+        print(f" given number {a} is greater then 20")
+
+print(f" given number {a} is less then 20")
+
+a=18
+if a>=16:
+    
+    print(f" given number {a} is greater then 20")
+
+print(f" given number {a} is less then 20")
+a=123
+if a>=16:
+    
+    print(f" given number {a} is greater then 20")
+else:
+    
+
+    print(f" given number {a} is less then 20")
+x=12
+if x%2==0:
+
+    print(f"{x} is even number")
+else:
+     print(f" {x} is odd number")
+x=18
+if x>11:
+    
+    if x%2==0:
+        print(f"{x} is even number")
+    else:
+        print(f" {x} is odd number")
+else:
+    print(f" given number {1} is less than 11")
+
+x="s"
+if x=="a":
+    print("a is vowel")
+elif x=="e":
+    print("e is vowel")
+elif x=="i":
+    print("i is vowel")
+else:
+    print("s is consonent")
+
+
