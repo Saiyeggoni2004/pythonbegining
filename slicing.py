7ee9e03dfbccdd9fa9c087a43d1ae24e57c9b3a3
Saiyeggoni2004 <yeggonisai@gmail.com>
2025-05-29 09:55:49 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,14 @@
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a[0:])
+print(a[0:-1])
+print(a[1:-1])
+print(a[3:-4])
+print(a[-1:6])
+print(a[-4:-1])
+print(a[-1:1])
+print(a[4:6:1])
+print(a[6:1:-2])
+print(a[1:1])
+print(a[-1:-1])
+
+print(a[-1:])
