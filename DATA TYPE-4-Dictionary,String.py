7926c29e09099bcd7a913a97ef6af968bd7f3896
Saiyeggoni2004 <yeggonisai@gmail.com>
2025-06-04 20:42:21 +0530

    Update and rename DATA TYPE-4-Dictionary.py to DATA TYPE-4-Dictionary,String.py

--- DATA TYPE-4-Dictionary,String.py
+++ DATA TYPE-4-Dictionary,String.py
@@ -1,4 +1,4 @@
-Dictionang
+Dictionary
 
 we use list, tuple and set to represent a group of individual objects as a single Entity
 If we want to represent a group of objects as kay-value pairs then we should go for dictionary.
@@ -7,3 +7,9 @@ Dictionaries can be created using pair of Curly braces({}).
 Each item in the dictionary consists of key, followed by a value.
 And each item is seperated using commma (,) insertion order is preserved.
 NOTE:Duplicate keys are not allowed but values are allowed.
+
+String
+Any sequnce of characters in single or doublr quotes is considered as string.
+NOTE:Accessing characters can be done in two ways 
+1).Indexing(both positive and negative)
+2).Slicing
