bfff82ea7e3da29061c01c879a2b8dbe2d1197c8
Saiyeggoni2004 <yeggonisai@gmail.com>
2025-06-04 20:37:55 +0530

    Add files via upload

---
+++ DATA TYPE-4-Dictionary.py
@@ -0,0 +1,9 @@
+Dictionang
+
+we use list, tuple and set to represent a group of individual objects as a single Entity
+If we want to represent a group of objects as kay-value pairs then we should go for dictionary.
+{key: Value}
+Dictionaries can be created using pair of Curly braces({}).
+Each item in the dictionary consists of key, followed by a value.
+And each item is seperated using commma (,) insertion order is preserved.
+NOTE:Duplicate keys are not allowed but values are allowed.
